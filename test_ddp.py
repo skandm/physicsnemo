09d6c2e0b727a63283f920e13c4c13b950b58ed1
skand mishra <skand@ntopology.com>
2026-05-13 21:08:49 -0400

    Update DDP test: configurable backend

--- test_ddp.py
+++ test_ddp.py
@@ -4,9 +4,13 @@ import os
 
 rank = int(os.environ['RANK'])
 local_rank = int(os.environ['LOCAL_RANK'])
-dist.init_process_group('nccl')
+world_size = int(os.environ['WORLD_SIZE'])
+
+backend = os.environ.get('DIST_BACKEND', 'nccl')
+dist.init_process_group(backend)
 torch.cuda.set_device(local_rank)
+
 t = torch.ones(1000, 1000, device='cuda')
 dist.all_reduce(t)
-print(f'Rank {rank}: OK, mean={t.mean():.1f}')
+print(f'Rank {rank}: all_reduce done, mean={t.mean().item():.1f}')
 dist.destroy_process_group()
